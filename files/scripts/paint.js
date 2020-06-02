427b5f822e3e2fd1af82b9c4636ced43dcff1912
LightninG8 <tov.kessler@yandex.ru>
2020-06-02 18:26:11 +0800

    Update paint.js

--- files/scripts/paint.js
+++ files/scripts/paint.js
@@ -8,58 +8,49 @@ let canvBottom = document.getElementById("canvBottom"),
     canvCorner = document.getElementById("canvCorner");
 
 
-canvCorner.addEventListener("mousedown", function (e) {
-    canvCorner.addEventListener("mousemove", function (e) {
+// canvCorner.addEventListener("mousedown", function (e) {
+//     canvCorner.addEventListener("mousemove", function (e) {
 
-    });
-    canvCorner.addEventListener("mouseup", function (e) {
+//     });
+//     canvCorner.addEventListener("mouseup", function (e) {
 
-    });
+//     });
 
-});
-
-
-
-// Logic
-let mousePos = {
-    x: 0,
-    y: 0
-};
-let isDraw = false;
+// });
 
 // Drawing
 canvas.addEventListener("mousedown", function (e) {
-    mousePos.x = e.pageX - this.offsetLeft;
-    mousePos.y = e.pageY - this.offsetTop;
+    let mousePos = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+    };
 
-    isDraw = true;
-
-    ctx.fill();
+    let isDraw = true;
 
     ctx.beginPath();
     ctx.moveTo(mousePos.x, mousePos.y);
-});
 
-canvas.addEventListener("mousemove", function (e) {
-    if (isDraw) {
-        mousePos.x = e.pageX - this.offsetLeft;
-        mousePos.y = e.pageY - this.offsetTop;
+    canvas.addEventListener("mousemove", function (e) {
+        if (isDraw) {
+            mousePos.x = e.pageX - this.offsetLeft;
+            mousePos.y = e.pageY - this.offsetTop;
 
-        ctx.lineWidth = 4;
-        ctx.lineCap = "round";
+            ctx.lineWidth = 4;
+            ctx.lineCap = "round";
 
-        ctx.lineTo(mousePos.x, mousePos.y);
-        ctx.stroke();
-        ctx.moveTo(mousePos.x, mousePos.y);
-    }
-});
+            ctx.lineTo(mousePos.x, mousePos.y);
+            ctx.stroke();
+            ctx.moveTo(mousePos.x, mousePos.y);
+        }
+    });
 
-canvas.addEventListener("mouseup", function (e) {
-    ctx.closePath();
-    isDraw = false;
-});
-canvas.addEventListener("mouseleave", function (e) {
-    ctx.closePath();
-    isDraw = false;
+    canvas.addEventListener("mouseup", function (e) {
+        ctx.closePath();
+        isDraw = false;
+    });
+    canvas.addEventListener("mouseleave", function (e) {
+        ctx.closePath();
+        isDraw = false;
+    });
 });
 // End drawing
