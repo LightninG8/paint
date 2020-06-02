4edea8e0fe6168c0ec923c3b25485ca7d21bc7e1
LightninG8 <tov.kessler@yandex.ru>
2020-06-03 02:22:38 +0800

    Add: resizing

--- files/scripts/paint.js
+++ files/scripts/paint.js
@@ -1,42 +1,115 @@
-// Canvas settings
+"use string";
+// Настройки канваса
 let canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
 
-// Resizing
-let canvBottom = document.getElementById("canvBottom"),
+canvas.width = 940;
+canvas.height = 540;
+// Конец Настройки канваса
+
+// Изменение размера
+let canvContainer = document.querySelector(".canvas"),
+    canvFrame = document.querySelector(".canvas__frame"),
+    canvBottom = document.getElementById("canvBottom"),
     canvRight = document.getElementById("canvRight"),
     canvCorner = document.getElementById("canvCorner");
 
+let isDraggable = false;
 
-// canvCorner.addEventListener("mousedown", function (e) {
-//     canvCorner.addEventListener("mousemove", function (e) {
+// Создаём данные изображения
+let imageData = ctx.createImageData(canvas.width, canvas.height);
 
-//     });
-//     canvCorner.addEventListener("mouseup", function (e) {
+// При нажатии на ползунок
+[canvBottom, canvRight, canvCorner].forEach((e) => {
+    e.addEventListener("mousedown", function (e) {
+        let direction = {
+            canvBottom: 0,
+            canvRight: 0,
+            canvCorner: 0
+        }
 
-//     });
+        canvFrame.style.display = 'block';
+        canvFrame.style.zIndex = 1000;
 
-// });
+        isDraggable = true;
 
-// Drawing
+        imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        // меняет рамку при событии движении мыши
+        function onMouseMove(e) {
+            if (isDraggable) {
+                // дополнительно учитывая изначальный сдвиг относительно указателя мыши
+                console.log(this);
+                if (this == canvBottom) {
+                    canvFrame.style.height = e.pageY - 5 - 92 + 'px';
+                } else if (this == canvRight) {
+                    canvFrame.style.width = e.pageX - 5 + 'px';
+                } else if (this == canvCorner) {
+                    canvFrame.style.width = e.pageX - 5 + 'px';
+                    canvFrame.style.height = e.pageY - 5 - 92 + 'px';
+                }
+            }
+        }
+        // зафиксировать рамку, удалить ненужные обработчики
+        function onMouseUp(e) {
+            if (isDraggable) {
+                isDraggable = false;
+
+                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mouseUp', onMouseUp);
+
+                if (this == canvBottom) {
+                    canvContainer.style.height = e.pageY - 5 - 92 + 'px';
+                } else if (this == canvRight) {
+                    canvContainer.style.width = e.pageX - 5 + 'px';
+                } else if (this == canvCorner) {
+                    canvContainer.style.width = e.pageX - 5 + 'px';
+                    canvContainer.style.height = e.pageY - 5 - 92 + 'px';
+                }
+
+                canvFrame.style.display = 'none';
+
+                // Канвас
+                canvas.width = parseInt(canvFrame.style.width);
+                canvas.height = parseInt(canvFrame.style.height);
+                ctx.putImageData(imageData, 0, 0);
+            }
+
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener("mouseup", onMouseUp);
+    });
+
+    e.ondragstart = function () {
+        return false;
+    };
+});
+// Конец Изменение размера
+
+// Рисование
 canvas.addEventListener("mousedown", function (e) {
     let mousePos = {
-        x: e.pageX - this.offsetLeft,
-        y: e.pageY - this.offsetTop
+        x: e.layerX,
+        y: e.layerY
     };
 
     let isDraw = true;
 
+    ctx.lineWidth = 1;
+    ctx.lineCap = "round";
+
     ctx.beginPath();
     ctx.moveTo(mousePos.x, mousePos.y);
+    ctx.lineTo(mousePos.x, mousePos.y);
+    ctx.stroke();
 
     canvas.addEventListener("mousemove", function (e) {
         if (isDraw) {
-            mousePos.x = e.pageX - this.offsetLeft;
-            mousePos.y = e.pageY - this.offsetTop;
-
-            ctx.lineWidth = 4;
-            ctx.lineCap = "round";
+            mousePos = {
+                x: e.layerX,
+                y: e.layerY
+            };
 
             ctx.lineTo(mousePos.x, mousePos.y);
             ctx.stroke();
@@ -53,4 +126,4 @@ canvas.addEventListener("mousedown", function (e) {
         isDraw = false;
     });
 });
-// End drawing
+// Конец Рисование
